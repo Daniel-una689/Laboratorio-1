cf85a230cfe9597eeb611cc9308586fef0e59140
danie <jose.brenes.campos@est.una.ac.c>
2026-08-19 23:36:11 -0600

    funcion de generar cola de compras

--- Tarea1.py
+++ Tarea1.py
@@ -4,7 +4,7 @@
 #creamos la clase del producto
 
 #para la validacion del id
-from unicodedata import name, numeric
+#from unicodedata import name, numeric
 
 
 class Product:
@@ -42,21 +42,19 @@ class Queue:
                 self.size =0
 
 
-            def is_empty(self):
-                return self.size == 0
+    def is_empty(self):
+        return self.size == 0
 
-            def enqueue(self, producto: Product):
-                new_node = NodeQueue(producto)
-                if self.is_empty():
-                    self.head = new_node
-                    self.tail = new_node
-                else:
-                    self.tail.next = new_node
-                    self.tail = new_node
-
-                self.size += 1
-
-                return ("Se agrego el producto a la cola")
+    def enqueue(self, producto: Product):
+        new_node = NodeQueue(producto)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
+        return ("Se agrego el producto a la cola")
 
 
 
@@ -78,44 +76,47 @@ class Linkedlist:
             self.size =0
 
 
-        def empty(self):
-            return self.size == 0
+    def empty(self):
+        return self.size == 0
     
-        def agregarProducto(self, producto: Product):
-            new_node = Node(producto)
-            if self.empty():
-                self.head = Node(producto) #basicamente en caso de que el nodo este vacio, se le asigna el valor del producto al nodo
-                self.tail = self.head  
+    def agregarProducto(self, producto: Product):
+        new_node = Node(producto)
+        if self.empty():
+            self.head = Node(producto) #basicamente en caso de que el nodo este vacio, se le asigna el valor del producto al nodo
+            self.tail = self.head  
 
-            else:
-                new_node.next  = self.head
-                self.head.anterior = new_node
-                self.head = new_node
+        else:
+            new_node.next  = self.head
+            self.head.previous = new_node
+            self.head = new_node
 
-            self.size += 1
+        self.size += 1
 
 
-        def deletefirst(self):
-            if self.empty():
-                print("La lista esta vacia")
-                return
+    def deletefirst(self):
+        if self.empty():
+            print("La lista esta vacia")
+            return
+        else:
 
             if self.head == self.tail:
                 self.head = None
                 self.tail = None
 
             else:
-                self.head.next = self.head
+                self.head = self.head.next
                 self.head.previous = None
-                self.size -= 1
 
-                return ("se elimino el primer producto de la lista")
+            self.size -= 1
 
-        def deletelast(self):
-            if self.empty():
-                print("La lista esta vacia")
-                return
+        return ("se elimino el primer producto de la lista")
 
+    def deletelast(self):
+        if self.empty():
+            print("La lista esta vacia")
+            return
+
+        else:
             if self.head == self.tail:
                 self.head = None
                 self.tail = None
@@ -123,77 +124,102 @@ class Linkedlist:
             else:
                 self.tail = self.tail.previous
                 self.tail.next = None
+
+            self.size -= 1
+
+            return ("se elimino el ultimo producto de la lista")
+
+
+    def deleteProduct(self,id):
+        if self.empty():
+            print("La lista esta vacia")
+            return
+
+
+
+        if not isinstance(id, (int, float)):
+            print("El id debe ser un numero")
+            return
+        
+            
+        if self.head.valor.id == id:
+            self.deletefirst()
+            return ("se elimino el primer producto de la lista")
+
+        if self.tail.valor.id == id:
+            self.deletelast()
+            return ("se elimino el ultimo producto de la lista")
+
+        current = self.head
+        for i in range(self.size):
+            if current.valor.id == id:
+                previous = current.previous
+                next = current.next
+
+                previous.next = next
+                next.previous = previous
+
                 self.size -= 1
-
-                return ("se elimino el ultimo producto de la lista")
-
-
-        def deleteProduct(self,id):
-            if self.empty():
-                print("La lista esta vacia")
-                return
-
-
-            if id is not numeric:
-                print("El id debe ser un numero")
-                return
-            
-            if self.head.valor.id == id:
-                self.deletefist()
-                return ("se elimino el primer producto de la lista")
-
-            if self.tail.valor.id == id:
-                self.deletelast()
-                return ("se elimino el ultimo producto de la lista")
-
-            current = self.head
-            for i in range(self.size):
-                if current.valor.id == id:
-                    previous = current.previous
-                    next = current.next
-
-                    previous.next = next
-                    next.previous = previous
-
-                    self.size -= 1
-                    return ("se elimino el producto con id: " + str(id))
-                else:
-                    current = current.next #ver si este ciclo esta bien integrado
-                if current is None:
-                     print("No se encontro el producto con id: " + str(id))
-                return
+                return ("se elimino el producto con id: " + str(id))
+            else:
+                current = current.next #ver si este ciclo esta bien integrado
+            if current is None:
+                print("No se encontro el producto con id: " + str(id))
+        return
 
 
             
 
-        def search(self, id):
-            if self.empty():
-                print("La lista esta vacia")
+    def search(self, id):
+        if self.empty():
+            print("La lista esta vacia")
+            return
+
+        if not isinstance(id, (int, float)):
+            print ("el numero de id debe de ser numerico")
+            return
+
+
+        current = self.head
+
+        for i in range(self.size):
+            if current.valor.id == id:
+                return current.valor
+            else:
+                current = current.next
+
+            if current is None:
+                print("No se encontro el producto con id: " + str(id))
                 return
-
-            if id is not numeric:
-                print ("el numero de id debe de ser numerico")
-                return
-
-
-            current = self.head
-
-            for i in range(self.size):
-                if current.valor.id == id:
-                    return current.valor
-                else:
-                    current = current.next
-
-                if current is None:
-                    print("No se encontro el producto con id: " + str(id))
-                    return
                 
         #en la clase del producto creamos un string el cual imprime el producto de manera ordenada y para hacer este metodo recursivo
         #hacemos que el mismo llame al mismo metodo de printRecursive para que le pase por parametro el nodo siguiente y asi sucesivamente hasta que llegue al final de la lista
-        def printRecursive(self, node):
-            if node is None:
-                return
-            else:
-                node.valor.Productprint()
-                self.printRecursive(node.next)
+    def printRecursive(self, node):
+        if node is None:
+            return
+        else:
+            node.valor.Productprint()
+            self.printRecursive(node.next)
+
+
+# debe recorrer la lista doblemente enlazada LinkedList, si producto.existence == 0, 
+# se agrega a la cola usando el metodo enqueue de la clase Queue.
+
+    def generarColaDeCompras(self):
+        if self.empty():
+            print("La lista está vacía")
+            return
+        else:
+            print("Generando cola de compras...")
+            cola = Queue()
+            current = self.head
+
+            while current is not None:
+                if current.valor.existence == 0:
+                    cola.enqueue(current.valor)
+                current = current.next 
+            return cola
+
+
+
 
